fc915b29f86ff4bccc5023860e12966893316dc7
unknown <quevedo.kevin@hotmail.com>
2023-10-23 14:22:45 -0300

    segundo-commit

---
+++ android/app/src/main/java/elbuencomer/app/MainActivity.java
@@ -0,0 +1,5 @@
+package elbuencomer.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'elbuencomer.app',
   appName: 'ElBuenComer',
   webDir: 'www',
   server: {
